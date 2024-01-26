ef4f23bc08559c3ebcd3ed6ee0fa8617692f1a2c
Yogesh Chandra Tripathi <137177000+yogeshchandratripathi@users.noreply.github.com>
2024-01-27 01:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(hasKey("red")){
+		console.log(true);
+	}
+	if(hasKey("green")){
+		console.log(true);
+	}
+	if (hasKey("white")) {
+		console.log(true);
+	}
+	else{
+		console.log(false);
+	}
 }
-
 // Do not change the code below
+
 const key = prompt("Enter Key.");
 alert(hasKey(key));
